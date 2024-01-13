71bb769f2a192fc304577fbc12f72bb91382b427
warlockja <warlockja@gmail.com>
2024-01-13 23:02:58 +0300

    testing github action revalidation

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
 // TAG revalidation
@@ -26,25 +26,3 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
-
-// // PATH revalidation
-// // e.g a webhook to `http://localhost:3000/api/revalidate?path=/&secret=<token>`
-// export async function GET(request: NextRequest) {
-//   const secret = request.nextUrl.searchParams.get("secret");
-//   const path = request.nextUrl.searchParams.get("path") || "/";
-
-//   if (secret !== process.env.REVALIDATION_TOKEN) {
-//     return NextResponse.json(
-//       { message: "Invalid secret" },
-//       {
-//         status: 401,
-//         statusText: "Unauthorized",
-//         headers: { "Content-Type": "Application/json" },
-//       }
-//     );
-//   }
-
-//   revalidatePath(path);
-
-//   return NextResponse.json({ revalidated: true, now: Date.now(), path });
-// }
